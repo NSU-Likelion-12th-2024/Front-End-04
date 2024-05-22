1d492d2bd5f3ddbb68776cd75a51170d9c614a91
sheepbottle1119 <sheepbottle1119@naver.com>
2024-05-22 17:51:37 +0900

    과제

---
+++ 양병현/test.js
@@ -0,0 +1,49 @@
+// console.log("로그 메세지");
+// // alert("경고창으로 출력");
+
+// let age = 25;
+// let pi = 3.14;
+
+// let name = "병현";
+// let message = "H3llo, world!";
+
+// let istrue = true;
+// let isFalse = false;
+
+// console.log(pi);
+
+// let person={
+//     name:'duc',
+//     age:23,
+//     city:'korea'};
+
+//     console.log(person.name);
+// for (let i = 0; i < 5; i++) {
+//   console.log("반복 횟수:", i);
+// }
+
+// for (let i=2*i-1; i<10; ){
+//     console.log("홀수의합:",i)
+// }
+
+// let num = 1000;
+
+// if (num > 90) {
+//   console.log("A입니다.");
+// } else if (num > 80) {
+//   console.log("b입니다.");
+// } else if (num > 70) {
+//   console.log("c입니다.");
+// } else {
+//   console.log("나머지는 f이다.");
+// }
+
+// for (i = 0; i < 5; i++) {
+//   console.log("반복횟수" + i);
+// }
+
+let i = 0;
+while (i < 5) {
+  console.log("while 반복 횟수", i);
+  i++;
+}
